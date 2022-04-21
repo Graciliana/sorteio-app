faec32ccd662ee8914fd58edefe96baf6c533703
Graciliana Kascher <graciliana.kascher@venturus.org.br>
2022-04-21 09:54:41 -0300

    exportando o app sorteio em java

---
+++ app/src/main/java/com/graciliana/app_sorteio/MainActivity.java
@@ -0,0 +1,30 @@
+package com.graciliana.app_sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sortearNumero(View view){
+
+         TextView texto = findViewById(R.id.txtResultado);
+
+         int numeroSorteiado = new Random().nextInt(11);
+
+         texto.setText("Número: " + numeroSorteiado );
+
+
+    }
+}
